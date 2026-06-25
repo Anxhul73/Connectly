55061deede035764104e10bb58cecae28a0a8af5
Anxhul73 <anshuljaswal9@gmail.com>
2026-06-25 10:44:29 +0530

    added basic CSS with Tailwind

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,45 +1,65 @@
 import { useState } from "react";
 import Sidebar from "../components/Sidebar";
+import { Trash2 } from "lucide-react"
+import { Plus } from "lucide-react"
 
 function CreatePost() {
-    const [ image, setImage ] = useState(null);
-    const [ preview, setPreview ] = useState("");
+    const [ images, setImages ] = useState([]);
+    const [ previews, setPreviews ] = useState([]);
     const [ caption, setCaption ]= useState("");
     const [ loading, setLoading ]= useState(false);
     const [ error, setError ] = useState("");
 
     const handleImageChange = (e) => {
-        const file = e.target.files[0];
+        const files = Array.from(e.target.files);
 
-        setImage(file);
+        setImages(files);
 
-        if (file){
-            setPreview(URL.createObjectURL(file));
-        }
+        if (!files.length) return;
+
+        setImages((prev) => [...prev, ...files]);
+
+        setPreviews((prev) => [
+            ...prev,
+            ...files.map(file => URL.createObjectURL(file)),
+        ]);
+    }
+
+    const handleImageDelete = (indexToDelete) => {
+        setImages((prev) => 
+            prev.filter((_, index) => index !== indexToDelete)
+        );
+
+        setPreviews((prev) => 
+            prev.filter((_, index) => index !== indexToDelete)
+        );
     }
 
     return (
         <div className="
             min-h-screen
-            bg-zinc-950flex
+            bg-zinc-950
             flex
         ">
             <Sidebar />
             <div className="
                 flex-1
+                min-h-screen
                 flex
                 justify-center
                 items-center
                 p-6
+                
             ">
                 <div className="
                 w-full
-                max-w-xl
+                max-w-2xl
                 bg-zinc-900
-                p-6
-                rounded-xl
+                p-8
+                rounded-2xl
                 border
                 border-zinc-800
+                
 
                 ">
                     <h1 className="
@@ -52,9 +72,11 @@ function CreatePost() {
                     
                     </h1>
 
-                    <div className="mt-6">
-                        <input 
+                    <div className="mt-6 ">
+                         <input 
                             type="file"
+                            multiple
+                            id="imageUpload"
                             accept= "image/*"
                             onChange={handleImageChange}
                             className="
@@ -66,36 +88,164 @@ function CreatePost() {
                                 p-3
                                 bg-zinc-800
                                 overflow-hidden
+                                hidden
 
                             " 
                         />
-                        {preview &&(
-                            <img
-                                src = {preview}
-                                alt= "preview"
-                                className = "
-                                    w-full
-                                    rounded-xl
-                                    mt-4
-                                    mah-h-[500px]
-                                    object-contain
-                                    bg-zinc-950
-                                "
+                        
+                        {previews.length === 0 && (
+                            <label 
+                            htmlFor="imageUpload"
+                            className="
+                                flex
+                                flex-col
+                                items-center
+                                justify-center
+                                gap-2
+                                w-full
+                                h-52
+                                border-2
+                                border-dashed
+                                border-zinc-700
+                                rounded-2xl
+                                cursor-pointer
+                                bg-zinc-900
+                                hover:border-indigo-500
+                                hover:bg-zinc-800
+                                transition-all
+                                duration-300
+                                
+                            "
+                        >
+                            <span className="text-3xl">📸</span>
+
+                            <p className="text white- font-medium">
+                                PNG, JPG, JPEG
+                            </p>
+                        </label>
+                        )} 
+                        
+                        {previews.length > 0 && (
+                            <div className="relative mt-4">
+
+                                <div
+                                  className={`grid gap-3 ${
+                                    previews.length === 1 ? "grid-cols-1" : "grid-cols-2"
+                                }`}
+                                >
+                                    {previews.map((preview, index) => (
+                                        <div
+                                            key={index}
+                                            className="relative overflow-hidden rounded-xl"
+                                        >
+                                            <img
+                                                src={preview}
+                                                alt={`preview-${index}`}
+                                                className="
+                                                w-full
+                                                    h-64
+                                                    object-contain
+                                                "
+                                            />
+
+                                            <button 
+                                                type = "button"
+                                                onClick={()=> handleImageDelete(index)}
+                                                className="
+                                                    absolute
+                                                    top-2
+                                                    right-2
+                                                    w-8
+                                                    h-8
+                                                    rounded-full
+                                                    bg-zinc-800
+                                                    text-white
+                                                    flex
+                                                    items-center
+                                                    justify-center
+                                                    hover:bg-zinc-700
+                                                    transition-all
+                                                    duration-300
+                                                "
+                                            >
+                                                <Trash2 size={16} />
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                                
+
+                            <input 
+                                type="file"
+                                multiple
+                                id="imageUploadFloating"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                                className="hidden"
                             />
-                        )}
+
+                            <label 
+                                htmlFor="imageUploadFloating"
+                                className="
+                                    absolute
+                                    bottom-4
+                                    right-4
+                                    w-12
+                                    h-12
+                                    rounded-full
+                                    bg-black/70
+                                    backdrop-blur-md
+                                    border
+                                    border-zinc-700
+                                    text-white
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:bg-indigo-500
+                                    transition-all
+                                    duration-300
+                                "
+                            >
+                                <Plus size={24}/>
+                            </label>
+                        </div>
+                    )}
                     </div>
-                    <div className="
-                w-full
-                max-w-xl
-                bg-zinc-900
-                p-6
-                rounded-xl
-                border
-                border-zinc-800
-                "
-                >
-                    
-                </div>
+                    <textarea 
+                        value={caption}
+                        onChange={(e) => setCaption(e.target.value)}
+                        placeholder="What's on your mind?"
+                        rows={3}
+                        maxLength={400}
+                        className="
+                            w-full
+                            bg-zinc-900
+                            mt-4
+                            p-6
+                            rounded-xl
+                            border
+                            border-zinc-800
+                            text-white
+                            resize-none
+                        "
+                    />
+
+                    <button 
+                        className="
+                            w-full
+                            mt-4
+                            py-3
+                            rounded-xl
+                            bg-indigo-600
+                            hover:bg-indigo-400
+                            text-white
+                            font-semibold
+                            transition-all
+                        "
+                    >
+                        Upload Post
+                    </button>
  
 
                 </div>
